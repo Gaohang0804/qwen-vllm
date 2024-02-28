843d1436185726d719495ce23ef72367ba09273d
Hang.Gao2 <Hang.Gao2@zeekrlife.com>
2024-02-28 14:19:24 +0800

    format

--- vllm_offline.py
+++ vllm_offline.py
@@ -3,15 +3,15 @@ from vllm_wrapper import vLLMWrapper
 model = "qwen/Qwen-7B-Chat-Int4"
 
 vllm_model = vLLMWrapper(model,
-                            quantization = 'gptq',
-                            dtype="float16",
-                            tensor_parallel_size=1,
-                            gpu_memory_utilization=0.6)
+                         quantization='gptq',
+                         dtype="float16",
+                         tensor_parallel_size=1,
+                         gpu_memory_utilization=0.6)
 
-history=None 
+history = None
 while True:
-    Q=input('提问:')
+    Q = input('提问:')
     response, history = vllm_model.chat(query=Q,
                                         history=history)
     print(response)
-    history=history[:20]
+    history = history[:20]
